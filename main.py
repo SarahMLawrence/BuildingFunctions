f69185a04c13cffcd008cd7a16117377aabb8f41
Sarah Lawrence <sarahlawrence1152@gmail.com>
2020-10-20 23:10:35 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+x = 5
+print('Hello')
+
+# def = Store
+def print_lyrics():
+  print("I'm a lumberjack, and I'm okay")
+  print("I sleep all night and I work all day.")
+
+print("Yo")
+
+# we invoke the function 
+# invoke = reuse
+print_lyrics()
+x = x + 2
+print(x)
